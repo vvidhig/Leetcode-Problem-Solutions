991fc29aa9f2309ff5def542937ea387ce29d0d6
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-01-13 02:00:26 +0530

    Create 33_SearchRotatedSortedArray.cpp

---
+++ Binary Search/33_SearchRotatedSortedArray.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        int mid;
+        while(left<=right)
+        {
+            mid = (left+right)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            //left part sorted
+            if(nums[left]<=nums[mid])
+            {
+                if(nums[left] <= target && target <= nums[mid])
+                {
+                    right = mid-1;
+                }
+                else
+                {
+                    left = mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid] <= target && target <= nums[right])
+                {
+                    left = mid+1;
+                }
+                else
+                {
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
