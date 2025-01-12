a39a8e55374e2c4ce4918597309cc22769c1e045
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-01-12 19:19:16 +0530

    Create GFG_FloorInSortedArray.cpp

---
+++ Binary Search/GFG_FloorInSortedArray.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    int findFloor(vector<int>& arr, int k) {
+        int n = arr.size();
+        int left = 0;
+        int right = n-1;
+        int ans = -1;
+        while(left<=right)
+        {
+            int mid = left + (right - left)/2;
+            if(k>=arr[mid])
+            {
+                ans = mid;
+                left = mid+1;
+            }
+            else
+            {
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        int ans = ob.findFloor(arr, k);
+
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
