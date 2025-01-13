bee5b845f773f29e94d2493721307b7d068d7a2c
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-01-13 13:23:18 +0530

    Create GFG_FindKthRotation.cpp

---
+++ Binary Search/GFG_FindKthRotation.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        int count = 0;
+        int n = arr.size();
+        for(int i = n-2; i>=0; i--)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                count = i+1;
+                break;
+            }
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        Solution ob;
+        int res = ob.findKRotation(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
