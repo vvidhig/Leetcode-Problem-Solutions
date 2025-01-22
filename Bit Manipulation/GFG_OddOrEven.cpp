733d18b906ce111734e8196d13b0bae5d5f42782
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-01-22 12:32:32 +0530

    Create GFG_OddOrEven.cpp

---
+++ Bit Manipulation/GFG_OddOrEven.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool isEven(int n) {
+        int lsb = (n >> 0)&1;
+        return lsb==1?false:true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        if (ob.isEven(N))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
