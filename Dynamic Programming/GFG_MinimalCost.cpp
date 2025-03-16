83b6459b8e5982ae3cebcb579ae6595a4158d446
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-03-16 22:14:07 +0530

    Create GFG_MinimalCost.cpp

---
+++ Dynamic Programming/GFG_MinimalCost.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i<n; i++)
+        {
+            for(int j = 1; j<=k; j++)
+            {
+                if(i-j>=0)
+                {
+                    dp[i] = min(dp[i], dp[i - j] + abs(arr[i] - arr[i - j]));
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
