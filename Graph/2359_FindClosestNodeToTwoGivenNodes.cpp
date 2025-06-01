ac8178fca2b8b95f7db39cc0ce1554dd7476f232
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-06-01 23:52:51 +0530

    Create 2359_FindClosestNodeToTwoGivenNodes.cpp

---
+++ Graph/2359_FindClosestNodeToTwoGivenNodes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void dfs(int current, int distance, const vector<int>& edges, vector<int>& distances)
+    {
+        while(current != -1 && distances[current] == -1)
+        {
+            distances[current] = distance++;
+            current = edges[current];
+        }
+    }
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) 
+    {
+        int res = -1, Min_of_Max = INT_MAX, n = edges.size();
+        vector<int> dist1(n, -1), dist2(n, -1);
+
+        dfs(node1, 0, edges, dist1);
+        dfs(node2, 0, edges, dist2);
+
+        for(int i = 0; i<n; i++)
+        {
+            if(dist1[i] >= 0 && dist2[i] >= 0)
+            {
+                int maxDist = max(dist1[i], dist2[i]);
+                if(maxDist < Min_of_Max)
+                {
+                    Min_of_Max = maxDist;
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
+};
