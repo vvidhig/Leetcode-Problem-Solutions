ff56bd86127755d0e1c212115d9f06bdccdb8660
Vidhi Gupta <vidhidoesnotreply@gmail.com>
2025-06-01 22:51:53 +0530

    Create 909_SnakesAndLadders.cpp

---
+++ Graph/909_SnakesAndLadders.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) 
+    {
+        int n = board.size();
+        int label = 1;
+        vector<pair<int, int>> cells(n*n+1);
+        vector<int> columns(n);
+        iota(columns.begin(), columns.end(), 0);
+
+        for(int row = n-1; row>=0; row--)
+        {
+            for(int col : columns)
+            {
+                cells[label++] = {row, col};
+            }
+            reverse(columns.begin(), columns.end());
+        }
+
+        vector<int> dist(n*n+1, -1);
+        dist[1] = 0;
+        queue<int> q;
+        q.push(1);
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            for(int move = 1; move <=6 && move + curr <= n*n; move+=1)
+            {
+                auto [r,c] = cells[curr + move];
+                int dest = board[r][c] == -1 ? move + curr : board[r][c];
+                if(dist[dest] == -1)
+                {
+                    dist[dest] = dist[curr]+1;
+                    q.push(dest);
+                }
+            }
+        }
+        return dist[n*n];
+    }
+};
